52b059323464a9e89621b0498b2b0b908fafefdd
ivesaidway2much <ivesaidway2much@gmail.com>
2013-06-23 18:12:29 -0400

    Some changes the create rotation function

--- Rotation.cpp
+++ Rotation.cpp
@@ -17,13 +17,13 @@ std::vector<Rotation> create_rotations(const std::string& rot_string)
   std::size_t str_sz=rot_string.size();
   std::size_t found=0;
   temp_rotation.startpos=0;
-  temp_rotation.endpos=str_sz;
-  
+  temp_rotation.endpos=str_sz;  
   temp_rotation.name=rot_string;
 
   while (found < str_sz)   
   {// create a rotation for each word in the string, and then add that rotation to the rotations vector.
     //loop invariant all spaces prior to the string position = size_t found have been located
+    std::cout<< rot_string.substr(found, str_sz - found)<<endl;
     found=rot_string.find_first_of(" ", found+1);
     temp_rotation.rotationpos=found;
     rotations.push_back(temp_rotation);
